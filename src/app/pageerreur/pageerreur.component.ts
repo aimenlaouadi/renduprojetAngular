dcda88e5bc4291cf3cfd2a31f3dab16a5a49cbcb
aimen laouadi <alaouadi@stagiaire-humanbooster.com>
2024-07-08 12:31:56 +0200

    Premier commit : Initialisation du projet

---
+++ src/app/pageerreur/pageerreur.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pageerreur',
+  standalone: true,
+  imports: [],
+  templateUrl: './pageerreur.component.html',
+  styleUrl: './pageerreur.component.css'
+})
+export class PageerreurComponent {
+
+}
